efb0229d3e30bc0946631f733c8a7b53f8159820
Bouke Haarsma <bouke@haarsma.eu>
2016-07-18 22:28:15 +0200

    Chacha20Poly1305 verify key size and fix algorithm ordering when encrypting

--- Sources/hap-server/ChaCha20Poly1305.swift
+++ Sources/hap-server/ChaCha20Poly1305.swift
@@ -10,6 +10,8 @@ class ChaCha20Poly1305 {
     let chacha20: ChaCha20
 
     init?(key: Data, nonce: Data) {
+        precondition(key.count == 32, "encryption key must be 256 bit")
+
         guard let chacha20 = ChaCha20(key: Array(key), iv: Array(nonce)) else {
             return nil
         }
@@ -45,15 +47,17 @@ class ChaCha20Poly1305 {
     }
 
     func encrypt(message: Data, add: Data = Data()) throws -> Data {
+        let encrypted = Data(try chacha20.encrypt(Array(message)))
+
         let polyMessage = add + Data(repeating: 0, count: (16 - (add.count % 16)) % 16) +
-            message + Data(repeating: 0, count: (16 - (message.count % 16)) % 16)  +
+            encrypted + Data(repeating: 0, count: (16 - (encrypted.count % 16)) % 16)  +
             Data(bytes: UInt64(add.count).bigEndian.bytes()) +
-            Data(bytes: UInt64(message.count).bigEndian.bytes())
+            Data(bytes: UInt64(encrypted.count).bigEndian.bytes())
 
         guard let computedMac = poly1305.authenticate(Array(polyMessage)) else {
             throw Error.InvalidMessageAuthenticator
         }
-
-        return Data(try chacha20.encrypt(Array(message))) + computedMac
+        
+        return encrypted + computedMac
     }
 }
