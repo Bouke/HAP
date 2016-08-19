6fb2777527e0c04385be0666d2ec6247126246e1
Bouke Haarsma <bouke@haarsma.eu>
2016-08-19 07:51:41 +0200

    Move CryptoSwift verification to separate test module

--- Sources/hap-server/main.swift
+++ Sources/hap-server/main.swift
@@ -35,7 +35,3 @@ server.listen()
 //withExtendedLifetime(server) {
 //    RunLoop.current.run()
 //}
-// see https://github.com/krzyzanowskim/CryptoSwift/issues/304
-//let c = Cryptographer(sharedKey: Data(hex: "352ce52e8d7a6b964c061faba60c806271ae47b1391c36169050f150fac5c770")!)
-//print(try c.decrypt(Data()))
-//print(try c.decrypt(Data(hex: "a800ab7faf90ffa83a889fb690db37b5f9e2d51c25a2bbc2c52b01264333d43022564aac46075deaaf4ac7852ebabf0c0ab99cdb486259afdc078ce431498cbdedf7fb4b92aabe49f121fd4650d5f39690364101d6a310e9e67135a63c06e1d4010b08b3be3c034ae6042092bcbed6fb2451005b93f01a4965663635f51a6e37567ddac5135d445342d2bc49277f238e66d651cebcc6cfd4802022738d932e495d457dc5bc23634ff86a9187a102a3491041200a3dbb16d79889")!))
