d079e3ef2fb7bd5fff2a5c7a7c77360309c031dc
Bouke Haarsma <bouke@haarsma.eu>
2016-08-30 20:50:06 +0200

    Clarified Door service accessories

--- Sources/HAP/Accessories/Door.swift
+++ Sources/HAP/Accessories/Door.swift
@@ -8,15 +8,19 @@ extension Accessory {
     }
 }
 
+public typealias CurrentPosition = Int
+
 public enum PositionState: Int, NSObjectConvertible {
     case decreasing = 0, increasing, stopped
 }
 
+public typealias TargetPosition = Int
+
 extension Service {
     public class Door: Service {
-        public let currentPosition = GenericCharacteristic<Int>(type: .currentPosition, permissions: [.read, .events])
+        public let currentPosition = GenericCharacteristic<CurrentPosition>(type: .currentPosition, permissions: [.read, .events])
         public let positionState = GenericCharacteristic<PositionState>(type: .positionState, permissions: [.read, .events])
-        public let targetPosition = GenericCharacteristic<Int>(type: .targetPosition)
+        public let targetPosition = GenericCharacteristic<TargetPosition>(type: .targetPosition)
         
         public init() {
             super.init(type: .door, characteristics: [currentPosition, positionState, targetPosition])
