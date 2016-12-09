f3b1fd1d60303ece286b83ddd4529fc9ebbba042
Bouke Haarsma <bouke@haarsma.eu>
2016-12-09 19:19:45 +0100

    Link to correct Evergreen major version

--- Package.swift
+++ Package.swift
@@ -11,6 +11,6 @@ let package = Package(
         .Package(url: "https://github.com/Bouke/CommonCrypto.git", majorVersion: 1),
         .Package(url: "https://github.com/Bouke/SRP.git", majorVersion: 1),
         .Package(url: "https://github.com/Bouke/HKDF.git", majorVersion: 1),
-        .Package(url: "https://github.com/Bouke/Evergreen.git", majorVersion: 1),
+        .Package(url: "https://github.com/Bouke/Evergreen.git", majorVersion: 0),
     ]
 )
