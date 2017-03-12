e68baea6e4aa3ef1bf3cb726b662c176401a17a9
Bouke Haarsma <bouke@haarsma.eu>
2017-03-12 14:15:49 +0100

    Updated Evergreen version for Linux

--- Package.swift
+++ Package.swift
@@ -10,6 +10,6 @@ let package = Package(
         .Package(url: "https://github.com/Bouke/CLibSodium.git", majorVersion: 1),
         .Package(url: "https://github.com/Bouke/SRP.git", majorVersion: 2),
         .Package(url: "https://github.com/Bouke/HKDF.git", majorVersion: 2),
-        .Package(url: "https://github.com/Bouke/Evergreen.git", majorVersion: 0),
+        .Package(url: "https://github.com/knly/Evergreen.git", majorVersion: 1),
     ]
 )
