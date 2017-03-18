72a87601214b65dd3b9624d370ae9b44dba77552
Bouke Haarsma <bouke@haarsma.eu>
2017-03-18 08:35:39 +0100

    Build against Kitura-net

--- Package.swift
+++ Package.swift
@@ -11,5 +11,6 @@ let package = Package(
         .Package(url: "https://github.com/Bouke/SRP.git", majorVersion: 2),
         .Package(url: "https://github.com/Bouke/HKDF.git", majorVersion: 2),
         .Package(url: "https://github.com/knly/Evergreen.git", majorVersion: 1),
+        .Package(url: "https://github.com/IBM-Swift/Kitura-net.git", majorVersion: 1, minor: 6),
     ]
 )
