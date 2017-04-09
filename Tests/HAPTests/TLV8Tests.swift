1bcf68cd5b14fbd6158e81abaf7f3af7568f6915
Bouke Haarsma <bouke@haarsma.eu>
2017-04-09 14:44:04 +0200

    TLV8 test

---
+++ Tests/HAPTests/TLV8Tests.swift
@@ -0,0 +1,35 @@
+@testable import HAP
+import HKDF
+import SRP
+import XCTest
+
+class TLV8Tests: XCTestCase {
+    static var allTests : [(String, (TLV8Tests) -> () throws -> Void)] {
+        return [
+            ("test", test),
+            ("testLinuxTestSuiteIncludesAllTests", testLinuxTestSuiteIncludesAllTests),
+        ]
+    }
+    
+    func test() {
+        let publicKey = Data(repeating: 0, count: 256) + Data(repeating: 1, count: 256) + Data(repeating: 2, count: 256)
+        let original: PairTagTLV8 = [
+            .publicKey: publicKey
+        ]
+        let encoded = encode(original)
+        let decoded: PairTagTLV8 = try! decode(encoded)
+        XCTAssertEqual(original, decoded)
+    }
+    
+    // from: https://oleb.net/blog/2017/03/keeping-xctest-in-sync/#appendix-code-generation-with-sourcery
+    func testLinuxTestSuiteIncludesAllTests() {
+        #if os(macOS) || os(iOS) || os(tvOS) || os(watchOS)
+            let thisClass = type(of: self)
+            let linuxCount = thisClass.allTests.count
+            let darwinCount = Int(thisClass
+                .defaultTestSuite().testCaseCount)
+            XCTAssertEqual(linuxCount, darwinCount,
+                           "\(darwinCount - linuxCount) tests are missing from allTests")
+        #endif
+    }
+}
--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -2,7 +2,7 @@ import XCTest
 @testable import HAPTests
 
 XCTMain([
-     testCase(PairSetupControllerTests.allTests),
     testCase(EndpointTests.allTests),
     testCase(PairSetupControllerTests.allTests),
+    testCase(TLV8Tests.allTests),
 ])
