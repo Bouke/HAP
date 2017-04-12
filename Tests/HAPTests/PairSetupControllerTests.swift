5441d9a623ca473194161a6a3a2f7029ef16fe22
Bouke Haarsma <bouke@haarsma.eu>
2017-04-12 14:36:11 +0200

    Fixed tests

--- Tests/HAPTests/PairSetupControllerTests.swift
+++ Tests/HAPTests/PairSetupControllerTests.swift
@@ -88,7 +88,7 @@ class PairSetupControllerTests: XCTestCase {
             try! Ed25519.verify(publicKey: response[.publicKey]!, message: hashOut, signature: response[.signature]!)
         }
         
-        XCTAssertEqual(device.clients[clientIdentifier], keys.publicKey)
+        XCTAssertEqual(device.pairings[clientIdentifier], keys.publicKey)
     }
 
     // from: https://oleb.net/blog/2017/03/keeping-xctest-in-sync/#appendix-code-generation-with-sourcery
