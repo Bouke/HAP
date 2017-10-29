5d48466367fb9389e8f1bee237cbb4dd9183e3e6
Bouke Haarsma <bouke@haarsma.eu>
2017-10-29 11:05:19 +0100

    Small refactor for readability

--- Sources/HAP/Endpoints/pairVerify().swift
+++ Sources/HAP/Endpoints/pairVerify().swift
@@ -17,40 +17,29 @@ func pairVerify(device: Device) -> Application {
         guard let sequence = data[.sequence]?.first.flatMap({ PairVerifyStep(rawValue: $0) }) else {
             return .badRequest
         }
-
-        switch sequence {
-        case .startRequest:
-            logger.info("Pair verify started")
-            let response: PairTagTLV8
-            let session: PairVerifyController.Session
-            do {
-                (response, session) = try controller.startRequest(data)
-            } catch {
-                logger.warning(error)
+        do {
+            switch sequence {
+            case .startRequest:
+                logger.info("Pair verify started")
+                let (response, session) = try controller.startRequest(data)
+                connection.context["hap.pairVerify.session"] = session
+                return Response(status: .ok, data: encode(response), mimeType: "application/pairing+tlv8")
+            case .finishRequest:
+                guard let session = connection.context["hap.pairVerify.session"] as? PairVerifyController.Session else {
+                    logger.warning("No session")
+                    return .badRequest
+                }
+                let result = try controller.finishRequest(data, session)
+                let response = UpgradeResponse(cryptographer: Cryptographer(sharedKey: session.sharedSecret))
+                response.headers["Content-Type"] = "application/pairing+tlv8"
+                response.body = encode(result)
+                return response
+            default:
                 return .badRequest
             }
-            connection.context["hap.pairVerify.session"] = session
-            return Response(status: .ok, data: encode(response), mimeType: "application/pairing+tlv8")
-
-        case .finishRequest:
-            guard let session = connection.context["hap.pairVerify.session"] as? PairVerifyController.Session else {
-                logger.warning("No session")
-                return .badRequest
-            }
-            let result: PairTagTLV8
-            do {
-                result = try controller.finishRequest(data, session)
-            } catch {
-                logger.warning(error)
-                return .badRequest
-            }
-            
-            let response = UpgradeResponse(cryptographer: Cryptographer(sharedKey: session.sharedSecret))
-            response.headers["Content-Type"] = "application/pairing+tlv8"
-            response.body = encode(result)
-            return response
-
-        default: return .badRequest
+        } catch {
+            logger.warning(error)
+            return .badRequest
         }
     }
 }
