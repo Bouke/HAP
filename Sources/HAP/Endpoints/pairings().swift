e95cb276a67b22375443ae1618720aaa2086a9ba
Bouke Haarsma <bouke@haarsma.eu>
2018-03-25 19:46:01 +0200

    Don’t require username in request for listing pairings

--- Sources/HAP/Endpoints/pairings().swift
+++ Sources/HAP/Endpoints/pairings().swift
@@ -4,6 +4,8 @@ import func Evergreen.getLogger
 fileprivate let logger = getLogger("hap.endpoints.pairings")
 
 func pairings(device: Device) -> Application {
+    let controller = PairingsController(device: device)
+
     return { connection, request in
         var body = Data()
         guard request.method == "POST" else {
@@ -13,8 +15,7 @@ func pairings(device: Device) -> Application {
             (try? request.readAllData(into: &body)) != nil,
             let data: PairTagTLV8 = try? decode(body),
             data[.state]?[0] == PairStep.request.rawValue,
-            let method = data[.pairingMethod].flatMap({ PairingMethod(rawValue: $0[0]) }),
-            let username = data[.identifier]
+            let method = data[.pairingMethod].flatMap({ PairingMethod(rawValue: $0[0]) })
             else {
                 return .badRequest
         }
@@ -32,7 +33,8 @@ func pairings(device: Device) -> Application {
 
         switch method {
         case .addPairing:
-            guard let publicKey = data[.publicKey],
+            guard let username = data[.identifier],
+                let publicKey = data[.publicKey],
                 let permissions = data[.permissions]?.first,
                 let role = Pairing.Role(rawValue: permissions) else {
                     return .badRequest
@@ -40,22 +42,15 @@ func pairings(device: Device) -> Application {
             device.add(pairing: Pairing(identifier: username, publicKey: publicKey, role: role))
             logger.info("Added \(role) pairing for \(String(data: username, encoding: .utf8)!)")
         case .removePairing:
+            guard let username = data[.identifier] else {
+                return .badRequest
+            }
             device.remove(pairingWithIdentifier: username)
             logger.info("Removed pairing for \(String(data: username, encoding: .utf8)!)")
         case .listPairings:
             logger.debug("Listing parings")
-            let pairings = device.configuration.pairings.values
-                .map { pairing -> PairTagTLV8 in
-                    [
-                        (.identifier, pairing.identifier),
-                        (.publicKey, pairing.publicKey),
-                        (.permissions, Data(bytes: [pairing.role.rawValue]))
-                    ]
-                }
-                .joined(separator: [(.state, Data(bytes: [PairStep.response.rawValue]))])
-                .flatMap { $0 }
-            let results: PairTagTLV8 = [(.state, Data(bytes: [PairStep.response.rawValue]))] + pairings
-            return Response(status: .ok, data: encode(results), mimeType: "application/pairing+tlv8")
+            let result = controller.listPairings()
+            return Response(status: .ok, data: encode(result), mimeType: "application/pairing+tlv8")
         default:
             logger.info("Unhandled PairingMethod request: \(method)")
             return .badRequest
