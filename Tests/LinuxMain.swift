652004e5823409bdfbcf0ae739cf374e3b218a2f
Bouke Haarsma <bouke@haarsma.eu>
2019-01-05 18:02:02 +0100

    Don’t reference disabled tests

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -4,8 +4,8 @@ import XCTest
 XCTMain([
     testCase(AccessoriesTests.allTests),
     testCase(DeviceTests.allTests),
-    testCase(EndpointTests.allTests),
-    testCase(PairingsEndpointTests.allTests),
+    // testCase(EndpointTests.allTests),
+    // testCase(PairingsEndpointTests.allTests),
     testCase(PairSetupControllerTests.allTests),
     testCase(PairVerifyControllerTests.allTests),
     testCase(TLV8Tests.allTests),
