77a94b62f9ed1480f31baf13229af522b9862ff5
Bouke Haarsma <bouke@haarsma.eu>
2020-01-31 09:01:01 +0100

    Require NIO 2.13 or higher with 32-bit patch

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         .package(url: "https://github.com/Bouke/HKDF.git", from: "3.1.0"),
         .package(url: "https://github.com/IBM-Swift/BlueCryptor.git", from: "1.0.21"),
         .package(url: "https://github.com/crossroadlabs/Regex.git", from: "1.1.0"),
-        .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.13.0"),
         .package(url: "https://github.com/apple/swift-log.git", Version("0.0.0") ..< Version("2.0.0")),
     ],
     targets: [
