83fbb3fc6d3d22676a5a00ad75eac239a7ba2f83
Bouke Haarsma <bouke@haarsma.eu>
2021-01-12 16:14:28 +0100

    Lower log level of encryption messages

--- Sources/HAP/Security/Cryptographer.swift
+++ Sources/HAP/Security/Cryptographer.swift
@@ -40,7 +40,7 @@ class Cryptographer {
     }
 
     func decrypt(length: Int, cipher: inout ByteBuffer, message: inout ByteBuffer) throws {
-        logger.info("Decrypt message #\(self.decryptCount), length: \(length)")
+        logger.debug("Decrypt message #\(self.decryptCount), length: \(length)")
         defer { decryptCount += 1 }
         var lengthBytes = cipher.readSlice(length: 2)!
         let nonce = decryptCount.bigEndian.bytes
@@ -48,7 +48,7 @@ class Cryptographer {
     }
 
     func encrypt(length: Int, plaintext: inout ByteBuffer, cipher: inout ByteBuffer) throws {
-        logger.info("Encrypt message #\(self.encryptCount), length: \(length)")
+        logger.debug("Encrypt message #\(self.encryptCount), length: \(length)")
         defer { encryptCount += 1 }
         cipher.writeInteger(Int16(length), endianness: Endianness.little, as: Int16.self)
         let additional = cipher.viewBytes(at: 0, length: 2)!
