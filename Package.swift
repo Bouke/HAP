c0c352e0ac13489761e003bc3014e0ddf8e80af1
Diggory Laycock <nospam@diggory.net>
2023-11-25 17:17:18 +0000

    Changed macOS platform requirement to 10_15

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "HAP",
     platforms: [
-        .macOS(.v11),
+        .macOS(.v10_15),
     ],
     products: [
         .library(name: "HAP", targets: ["HAP"]),
